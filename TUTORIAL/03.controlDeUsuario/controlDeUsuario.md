7968d4ccef9011903bd58c24930607a167a9d020
chon-94 <107455342+chon-94@users.noreply.github.com>
2022-08-09 11:23:44 -0500

    control de usuario

---
+++ TUTORIAL/03.controlDeUsuario/controlDeUsuario.md
@@ -0,0 +1,50 @@
+# control de usuario
+Ahora necesitamos poder hacer que el personaje se mueva.
+
+Primero, en la parte superior del programa, agregue una constante que controla cuántos píxeles por actualización viaja nuestro personaje:
+
+    # Velocidad de movimiento del jugador, en píxeles por fotograma
+    VELOCIDAD_DEL _PERSONAJE = 5
+
+Luego, al final de nuestro método de configuración, necesitamos crear un motor de física que mueva a nuestro jugador y evite que atraviese las paredes como los fantasmas. 
+La clase PhysicsEngineSimple toma dos parámetros: 
+el sprite en movimiento y una lista de sprites por los que el sprite en movimiento no puede moverse.
+
+    # Crea fisicas
+    self.physics_engine = arcade.PhysicsEngineSimple(self.personaje_sprite, self.escena.get_sprite_list("pared "))
+
+Cada sprite tiene atributos Center_X y Center_Y. Cambiar estos cambiará la ubicación del sprite. 
+(También hay atributos para la parte superior, inferior, izquierda, derecha y ángulo que moverán el sprite).
+
+Cada sprite tiene variables Change_X y Change_Y. Estos se pueden usar para mantener la velocidad con la que se mueve el sprite. Los ajustaremos en función de la clave que golpea el usuario. Si el usuario presenta la tecla de flecha correcta, queremos un valor positivo para Change_x. Si el valor es 5, moverá 5 píxeles por cuadro.
+
+En este caso, cuando el usuario presiona una tecla, cambiaremos los sprites cambian x e y. El motor de física lo verá y moverá al jugador a menos que golpee una pared.
+
+    def on_key_press(self, key, modifiers):
+        """cada vez que se presiona una tecla."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.personaje_sprite.change_y = VELOCIDAD_DEL _PERSONAJE
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.personaje_sprite.change_y = -VELOCIDAD_DEL _PERSONAJE
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.personaje_sprite.change_x = -VELOCIDAD_DEL _PERSONAJE
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.personaje_sprite.change_x = VELOCIDAD_DEL _PERSONAJE
+
+Cuando soltamo la tecla, volveremos a poner nuestra velocidad a cero.
+
+    def on_key_release(self, key, modifiers):
+        """cuando el usuario suelta una tecla."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.personaje_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.personaje_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.personaje_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.personaje_sprite.change_x = 0
+
+Nuestro método on_update se llama unas 60 veces por segundo. Le pediremos al motor de física que mueva a nuestro jugador en función de su cambio_x y cambio_y.
+
--- TUTORIAL/03.controlDeUsuario/controlDeUsuario.py
+++ TUTORIAL/03.controlDeUsuario/controlDeUsuario.py
@@ -10,6 +10,9 @@ ESCALA_PERSONAJE = 1
 ESCALA_PAREDES = 0.5
 ESCALA_CAJAS = 0.7
 
+# Velocidad de movimiento del jugador, en píxeles por fotograma
+VELOCIDAD_DEL_PERSONAJE = 5
+
 class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
 
     def __init__(self):# Llama a la clase principal y configure la ventana
@@ -21,11 +24,16 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
         #self.pared_lista = None
         #self.caja_lista = None
         #self.personaje_lista = None
-        
-        self.escena = None # Nuestro objeto de escena
+
+        self.escena_0 = None # Nuestro objeto de escena
+        self.escena_1 = None # Nuestro objeto de escena
 
         # Variable separada que contiene el sprite del jugador
         self.personaje_sprite = None
+
+        # Motor de físicas
+        self.motor_de_física_0 = None
+        self.motor_de_física_1 = None
 
         arcade.set_background_color(arcade.csscolor.MIDNIGHT_BLUE) # Para darle color al fondo
         #arcade.set_background_color(arcade.color.DARK_SLATE_GRAY) # Para darle color al fondo
@@ -33,16 +41,18 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
     def setup(self):# Configuracion del juego, si llamas a esta funcion se reinicia el juego 
         
         # iniciar escena
-        self.escena = arcade.Scene()
+        self.escena_0 = arcade.Scene()
+        self.escena_1 = arcade.Scene()
+
         # Create the Sprite lists
-        self.escena.add_sprite_list("personaje")
-        self.escena.add_sprite_list("caja", use_spatial_hash=True)
-        self.escena.add_sprite_list("pared", use_spatial_hash=True)
+        #self.escena.add_sprite_list("personaje")
+        #self.escena.add_sprite_list("caja", use_spatial_hash=True)
+        #self.escena.add_sprite_list("pared", use_spatial_hash=True)
 
         # Crear las listas de Sprite
-        self.personaje_lista = arcade.SpriteList()
-        self.caja_lista = arcade.SpriteList(use_spatial_hash=True)
-        self.pared_lista = arcade.SpriteList(use_spatial_hash=True)
+        #self.personaje_lista = arcade.SpriteList()
+        #self.caja_lista = arcade.SpriteList(use_spatial_hash=True)
+        #self.pared_lista = arcade.SpriteList(use_spatial_hash=True)
 
         # Configura al jugador, colocándolo específicamente en estas coordenadas.
         imagen_personaje = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png" # Variable del personaje
@@ -50,8 +60,7 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
         self.personaje_sprite.center_x = 40  # Posision en el eje x    
         self.personaje_sprite.center_y = 128 # Posision en el eje y
         #self.personaje_lista.append(self.personaje_sprite) # Agregamos el sprite
-
-        self.escena.add_sprite("personaje", self.personaje_sprite)
+        self.escena_0.add_sprite("personaje", self.personaje_sprite)
 
         # Crear el suelo
         for x in range(0, 1350, 64): # range(Inicio,Fin,Separacion)
@@ -59,7 +68,7 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
             pared.center_x = x  # Posision en el eje x
             pared.center_y = 32 # Posision en el eje y
             #self.pared_lista.append(pared) # Agregamos la pared
-            self.escena.add_sprite("pared", pared)
+            self.escena_0.add_sprite("pared", pared)
 
         # Crear obstaculos
         cordenada_listas_cajas = [[256, 105], [456, 105], [656, 105], [856, 105], [1056, 105]]# Lista de coordenadas para sprites
@@ -67,8 +76,11 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
             caja = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", ESCALA_CAJAS )# Variable de la caja
             caja.position = cordenada # Decimos que la posision de la caja sera  el de la cordenada 
             #self.caja_lista.append(caja) # Agregamos la caja
-            self.escena.add_sprite("caja", caja)
+            self.escena_1.add_sprite("caja", caja)
 
+        # Crear el 'motor de física'
+        self.motor_de_física_0 = arcade.PhysicsEngineSimple(self.personaje_sprite, self.escena_0.get_sprite_list("pared"))
+        self.motor_de_física_1 = arcade.PhysicsEngineSimple(self.personaje_sprite, self.escena_1.get_sprite_list("caja"))
 
     def on_draw(self):# Renderizado de pantalla
 
@@ -79,7 +91,36 @@ class MyGame(arcade.Window):# Esta es la clase de la aplicacion principal
         #self.personaje_lista.draw()
         
         # Dibuja nuestra Escena
-        self.escena.draw()
+        self.escena_0.draw()
+        self.escena_1.draw()
+
+    def on_key_press(self, key, modifiers):# Cuando presionamos una tecla
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.personaje_sprite.change_y = VELOCIDAD_DEL_PERSONAJE
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.personaje_sprite.change_y = -VELOCIDAD_DEL_PERSONAJE
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.personaje_sprite.change_x = -VELOCIDAD_DEL_PERSONAJE
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.personaje_sprite.change_x = VELOCIDAD_DEL_PERSONAJE
+
+    def on_key_release(self, key, modifiers):# Cuando soltamos la tecla presionada
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.personaje_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.personaje_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.personaje_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.personaje_sprite.change_x = 0
+
+    def on_update(self, delta_time):# Movimiento y lógica de juego.
+
+        # Mueve al jugador con el motor de física.
+        self.motor_de_física_0.update()
+        self.motor_de_física_1.update()
 
 def main():# Funcion principal
     ventana = MyGame()
